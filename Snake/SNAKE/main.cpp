9dc8942564d0f0c6dbee66d5e1836a350f14f542
Elshtein99 <57881656+Elshtein99@users.noreply.github.com>
2019-11-27 15:47:51 +1300

    Main Change

--- Snake/SNAKE/main.cpp
+++ Snake/SNAKE/main.cpp
@@ -3,7 +3,7 @@
 #include "DEFINITIONS.hpp"
 #include <Windows.h>
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+int main() {
 		SNAKY::Game game = SNAKY::Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
 		game.Run();
 		return EXIT_SUCCESS;
